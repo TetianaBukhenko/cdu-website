daff59bfbfa04897bec27516e1684dc2a57f97fa
Tetiana <tania04bukhenko@gmail.com>
2024-09-30 01:37:32 +0300

    Updates

--- src/pages/NewsArticle/NewsArticle.tsx
+++
@@ -1,3 +0,0 @@
-export const NewsArticle = () => {
-  return (<h1>News Article</h1>);
-}
--- src/types/News.ts
+++
@@ -1,8 +0,0 @@
-export type News = {
-  id: number,
-  img: string,
-  type: 'news' | 'report' | 'event',
-  title: string,
-  date: Date,
-  text: string,
-}
